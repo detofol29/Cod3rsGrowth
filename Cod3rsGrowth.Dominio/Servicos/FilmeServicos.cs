fec03e751d76cf36a17aa1ef1492cf3c6178544e
Gabriel Detofol <detofol29@gmail.com>
2024-05-20 08:23:21 -0300

    Adiciona arquivos não rastreados antes do pull

---
+++ Cod3rsGrowth.Dominio/Servicos/FilmeServicos.cs
@@ -0,0 +1,21 @@
+﻿using Cod3rsGrowth.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Dominio.Servicos;
+
+public class FilmeServicos : IFilmeServico
+{
+    List<IAtor> IFilmeServico.ObterAtoresDoFilme(Filme filme)
+    {
+        var list = new List<IAtor>();
+        foreach(var ator in filme.Atores)
+        {
+            list.Add(ator);
+        }
+        return list;
+    }
+}
