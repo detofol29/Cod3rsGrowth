02c68b456a17bb573e79da4fe9691c5d1528db3e
Gabriel Detofol <detofol29@gmail.com>
2024-05-22 13:56:39 -0300

    Criando as classes Singleton Filme e Ator

---
+++ Cod3rsGrowth.Teste/ClassesSingleton/TabelaAtor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Cod3rsGrowth.Dominio.Modelos;
+
+namespace Cod3rsGrowth.Teste.ClassesSingleton;
+
+public class TabelaAtor
+{
+    private static TabelaAtor _instancia;
+
+    private List<Ator> _atores;
+
+    private TabelaAtor()
+    {
+        _atores = new List<Ator>
+        {
+            new() {Id = 1, Nome = "Samuel L. Jackson", IdFilme = 3},
+            new() {Id = 2, Nome = "Ewan McGregor", IdFilme = 3},
+            new() {Id = 3, Nome = "Natalie Portman", IdFilme = 3},
+            new() {Id = 4, Nome = "Michael J. Fox", IdFilme = 1},
+            new() {Id = 5, Nome = "Leonardo DiCaprio", IdFilme = 2}
+        };
+    }
+
+    public static TabelaAtor Instance
+    {
+        get
+        {
+            if(_instancia == null)
+            {
+                _instancia = new TabelaAtor();
+            }
+            return _instancia;
+        }
+    }
+
+    public List<Ator> Atores => _atores;
+}
---
+++ Cod3rsGrowth.Teste/ClassesSingleton/TabelaFilme.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Cod3rsGrowth.Dominio.Modelos;
+
+namespace Cod3rsGrowth.Teste.ClassesSingleton;
+
+public class TabelaFilme
+{
+    // Instância única da classe, inicializada como null
+    private static TabelaFilme _instancia;
+
+    private List<Filme> _filmes;
+
+    //Construtor privado para evitar instancia paralela
+    private TabelaFilme()
+    {
+        _filmes = new List<Filme>
+        {
+            new Filme { Id = 1, Titulo = "De Volta Para o Futuro", Genero = GeneroEnum.Ficcao },
+            new Filme { Id = 2, Titulo = "Titanic", Genero = GeneroEnum.Romance },
+            new Filme { Id = 3, Titulo = "Star Wars", Genero = GeneroEnum.Ficcao}
+        };
+    }
+        //Faz a instancia da TabelaFilmes 
+        public static TabelaFilme Instance
+        {
+            get
+            {
+                if (_instancia == null)
+                {
+                    _instancia = new TabelaFilme();
+                }
+                return _instancia;
+            }
+        }
+    public List<Filme> Filmes => _filmes;
+}
