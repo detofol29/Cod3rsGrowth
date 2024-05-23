f5c31d1d276fff9b63827b6206fa2495e464b97f
Gabriel Detofol <detofol29@gmail.com>
2024-05-23 08:36:09 -0300

    Refatorando a classe filmeRepositorioMock

--- Cod3rsGrowth.Teste/FilmeRepositorioMock.cs
+++ Cod3rsGrowth.Teste/FilmeRepositorioMock.cs
@@ -4,18 +4,26 @@ using Cod3rsGrowth.Servicos.Interfaces;
 using Cod3rsGrowth.Infra;
 using Microsoft.Extensions.DependencyInjection;
 using Cod3rsGrowth.Dominio.Modelos;
+using Cod3rsGrowth.Teste.ClassesSingleton;
 
 namespace Cod3rsGrowth.Teste;
 
 public class FilmeRepositorioMock : IFilmeRepositorio
 {
+    private readonly TabelaFilme _tabelaFilme;
+
+    public FilmeRepositorioMock()
+    {
+        _tabelaFilme = TabelaFilme.Instance;
+    }
+
     public Filme ObterPorId(int id)
     {
-       throw new NotImplementedException();
+        return _tabelaFilme.Filmes.FirstOrDefault(a => a.Id == id);
     }
 
     public List<Filme> ObterTodos() 
     {
-        return new List<Filme>();
+        return _tabelaFilme.Filmes;
     }
 }
