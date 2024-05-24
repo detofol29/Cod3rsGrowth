036dd21ef6741dbffe3951d5b557969bfd134bc8
Gabriel Detofol <detofol29@gmail.com>
2024-05-24 13:30:45 -0300

    refatorando a classe tabelasSingleton usando Lazy

--- Cod3rsGrowth.Teste/ClassesSingleton/TabelasSingleton.cs
+++ Cod3rsGrowth.Teste/ClassesSingleton/TabelasSingleton.cs
@@ -1,60 +1,20 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Cod3rsGrowth.Dominio.Modelos;
 
 namespace Cod3rsGrowth.Teste.ClassesSingleton;
 
 public class TabelasSingleton
 {
-    private static List<Filme> instanciaFilme;
-    private static List<Ator> instanciaAtor;
-    private static List<Usuario> instanciaUsuario;
-
-
-    private List<Filme> filmes;
-    private List<Ator> atores;
-    private List<Usuario> usuarios;
+    private static readonly Lazy<List<Filme>> instanciaFilme = new(() => new List<Filme>());
+    private static readonly Lazy<List<Ator>> instanciaAtor = new(() => new List<Ator>());
+    private static readonly Lazy<List<Usuario>> instanciaUsuario = new(() => new List<Usuario>());
 
     private TabelasSingleton()
     {
-        filmes = new List<Filme>();
-        atores = new List<Ator>();
-        usuarios = new List<Usuario>();
     }
-    public static List<Filme> InstanceFilme
-    {
-        get
-        {
-            if(instanciaFilme == null)
-            {
-                instanciaFilme = new TabelasSingleton().Filmes;
-            }
-            return instanciaFilme;
-        }
-    }
-    
-    public static List<Ator> InstanceAtor
-    {
-        get
-        {
-            if(instanciaAtor == null)
-            {
-                instanciaAtor = new TabelasSingleton().Atores;
-            }
-            return instanciaAtor;
-        }
-    }
-    public static List<Usuario> InstanceUsuario
-    {
-        get
-        {
-            if(instanciaUsuario == null)
-            {
-                instanciaUsuario = new TabelasSingleton().Usuarios;
-            }
-            return instanciaUsuario;
-        }
-    }
-    public List<Filme> Filmes => filmes;
-    public List<Ator> Atores => atores;
-    public List<Usuario>Usuarios => usuarios;
+
+    public static List<Filme> ObterInstacniaFilmes => instanciaFilme.Value;
+    public static List<Ator> ObterInstanciaAtores => instanciaAtor.Value;
+    public static List<Usuario> ObterInstanciaUsuarios => instanciaUsuario.Value;
 }
