9df8db472467c1f414797b71bbb77d6e738c81a8
Gabriel Detofol <detofol29@gmail.com>
2024-05-24 11:59:26 -0300

    Criando a classe TabelasSingletonLazy em paralelo para avaliação

--- Cod3rsGrowth.Teste/ClassesSingleton/TabelasSingleton.cs
+++ Cod3rsGrowth.Teste/ClassesSingleton/TabelasSingleton.cs
@@ -5,42 +5,56 @@ namespace Cod3rsGrowth.Teste.ClassesSingleton;
 
 public class TabelasSingleton
 {
-    private static TabelasSingleton instancia;
+    private static List<Filme> instanciaFilme;
+    private static List<Ator> instanciaAtor;
+    private static List<Usuario> instanciaUsuario;
+
 
     private List<Filme> filmes;
     private List<Ator> atores;
     private List<Usuario> usuarios;
 
-    public List<Ator> AdicionarAtoresAoFilme(int idFilme)
-    {
-        var list = new List<Ator>();
-        foreach (var ator in atores)
-        {
-            if (ator.IdFilme == idFilme)
-            {
-                list.Add(ator);
-            }
-        }
-        return list;
-    }
     private TabelasSingleton()
     {
         filmes = new List<Filme>();
         atores = new List<Ator>();
         usuarios = new List<Usuario>();
     }
-    public static TabelasSingleton Instance
+    public static List<Filme> InstanceFilme
     {
         get
         {
-            if (instancia == null)
+            if(instanciaFilme == null)
             {
-                instancia = new TabelasSingleton();
+                instanciaFilme = new TabelasSingleton().Filmes;
             }
-            return instancia;
+            return instanciaFilme;
+        }
+    }
+    
+    public static List<Ator> InstanceAtor
+    {
+        get
+        {
+            if(instanciaAtor == null)
+            {
+                instanciaAtor = new TabelasSingleton().Atores;
+            }
+            return instanciaAtor;
+        }
+    }
+    public static List<Usuario> InstanceUsuario
+    {
+        get
+        {
+            if(instanciaUsuario == null)
+            {
+                instanciaUsuario = new TabelasSingleton().Usuarios;
+            }
+            return instanciaUsuario;
         }
     }
     public List<Filme> Filmes => filmes;
     public List<Ator> Atores => atores;
-    public List<Usuario> Usuarios => usuarios;
+    public List<Usuario>Usuarios => usuarios;
 }
