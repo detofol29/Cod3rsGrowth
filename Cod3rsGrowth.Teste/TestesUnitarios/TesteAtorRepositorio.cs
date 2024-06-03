bfc83f40c3a541f97bcab7f80b05fa7e5c227eda
Gabriel Detofol <detofol29@gmail.com>
2024-06-03 13:51:22 -0300

    (criar-metodo-obter-todos-com-tdd) Separando Testes de repositorios em arquivos diferentes #Time 1h

--- Cod3rsGrowth.Teste/TestesUnitarios/TesteAtorRepositorio.cs
+++ Cod3rsGrowth.Teste/TestesUnitarios/TesteAtorRepositorio.cs
@@ -5,50 +5,16 @@ using Cod3rsGrowth.Infra.Interfaces;
 
 namespace Cod3rsGrowth.Teste.TestesUnitarios;
 
-public class TesteRepositorios : TesteBase
+public class TesteAtorRepositorio : TesteBase
 {
-    private readonly IFilmeRepositorio filmeRepositorio;
     private readonly IAtorRepositorio atorRepositorio;
-    private readonly IUsuarioRepositorio usuarioRepositorio;
-    public TesteRepositorios()
+    public TesteAtorRepositorio()
     {
-        filmeRepositorio = serviceProvider.GetService<IFilmeRepositorio>() ?? throw new Exception("Repositorio nao encontrado");
         atorRepositorio = serviceProvider.GetService<IAtorRepositorio>() ?? throw new Exception("Repositorio nao encontrado");
-        usuarioRepositorio = serviceProvider.GetService<IUsuarioRepositorio>() ?? throw new Exception("Repositorio nao encontrado");
     }
 
     [Fact]
-    public void obter_todos_retorna_lista_de_todos_os_filmes_do_repositorio_quando_lista_nao_nula()
-    {
-        filmeRepositorio.Inserir(new Filme { Id = 1, Titulo = "De Volta Para o Futuro", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre});
-        filmeRepositorio.Inserir(new Filme { Id = 2, Titulo = "Titanic", Genero = GeneroEnum.Romance, Classificacao = ClassificacaoIndicativa.doze});
-        filmeRepositorio.Inserir(new Filme { Id = 3, Titulo = "Star Wars", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre});
-        filmeRepositorio.Inserir(new Filme { Id = 4, Titulo = "O Senhor dos Anéis: A Sociedade do Anel", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze});
-        filmeRepositorio.Inserir(new Filme { Id = 5, Titulo = "O Senhor dos Anéis: As Duas Torres", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze});
-        filmeRepositorio.Inserir(new Filme { Id = 6, Titulo = "O Senhor dos Anéis: O Retorno do Rei", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze});
-        filmeRepositorio.Inserir(new Filme { Id = 7, Titulo = "Matrix", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.dezesseis});
-        filmeRepositorio.Inserir(new Filme { Id = 8, Titulo = "Gladiador", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.dezesseis});
-        filmeRepositorio.Inserir(new Filme { Id = 9, Titulo = "O Poderoso Chefão", Genero = GeneroEnum.Drama, Classificacao = ClassificacaoIndicativa.dezoito});
-        filmeRepositorio.Inserir(new Filme { Id = 10, Titulo = "Forrest Gump", Genero = GeneroEnum.Drama, Classificacao = ClassificacaoIndicativa.quatorze});
-        filmeRepositorio.Inserir(new Filme { Id = 11, Titulo = "Pulp Fiction", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.dezoito});
-        filmeRepositorio.Inserir(new Filme { Id = 12, Titulo = "O Cavaleiro das Trevas", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.quatorze});
-        var listaEsperada = TabelasSingleton.ObterInstanciaFilmes;
-        var titulo = listaEsperada[0].Titulo;
-        var id = listaEsperada[0].Id;
-        var genero = listaEsperada[0].Genero;
-        var classificacao = listaEsperada[0].Classificacao;
-
-        var lista = filmeRepositorio.ObterTodos();
-
-        Assert.NotEmpty(lista);
-        Assert.Equal(lista[0].Titulo, titulo);
-        Assert.Equal(lista[0].Id, id);
-        Assert.Equal(lista[0].Genero, genero);
-        Assert.Equal(lista[0].Classificacao, classificacao);
-        Assert.Equal(listaEsperada.Count(), lista.Count());
-    }
-    [Fact]
-    public void obter_todos_retorna_lista_de_todos_os_atores_do_repositorio_quando_lista_nao_nula()
+    public void ao_ObterTodos_retorna_lista_com_trinta_e_oito_atores()
     {
         atorRepositorio.Inserir(new() {Id = 1, Nome = "Samuel L. Jackson", IdFilme = 3, Premios = new List<string> { "SAG Awards", "Black Reel Awards"}});
         atorRepositorio.Inserir(new() {Id = 2, Nome = "Ewan McGregor", IdFilme = 3, Premios = new List<string> { "SAG Awards", "Black Reel Awards" }});
@@ -89,35 +55,10 @@ public class TesteRepositorios : TesteBase
         atorRepositorio.Inserir(new() {Id = 37, Nome = "Heath Ledger", IdFilme = 12, Premios = new List<string> { "SAG Awards", "Black Reel Awards"}});
         atorRepositorio.Inserir(new() {Id = 38, Nome = "Aaron Eckhart", IdFilme = 12, Premios = new List<string> { "SAG Awards", "Black Reel Awards"}});
         var listaEsperada = TabelasSingleton.ObterInstanciaAtores;
-        var nome = listaEsperada[0].Nome;
-        var id = listaEsperada[0].Id;
 
         var lista = atorRepositorio.ObterTodos();
 
         Assert.NotEmpty(lista);
-        Assert.Equal(nome, lista[0].Nome);
-        Assert.Equal(id, lista[0].Id);
-    }
-
-    [Fact]
-    public void obter_todos_retorna_lista_de_todos_os_usuarios_do_repositorio_quando_lista_nao_nula()
-    {
-        usuarioRepositorio.Inserir(new() {Nome = "Gabriel Detofol", IdUsuario = 1, Plano = PlanoEnum.Premium, Senha = "123", NickName = "Detofol29"});
-        usuarioRepositorio.Inserir(new() {Nome = "Marcos Paulo", IdUsuario = 2, Plano = PlanoEnum.Nerd, Senha = "123", NickName = "SilvaMarcosPaulo"});
-        usuarioRepositorio.Inserir(new() {Nome = "Rubens", IdUsuario = 3, Plano = PlanoEnum.Kids, Senha = "123", NickName = "DarthRubens"});
-        usuarioRepositorio.Inserir(new() {Nome = "André", IdUsuario = 4, Plano = PlanoEnum.Free, Senha = "123", NickName = "AndrezinDoGrau"});
-        var listaEsperada = TabelasSingleton.ObterInstanciaUsuarios;
-        var nome = listaEsperada[0].Nome;
-        var id = listaEsperada[0].IdUsuario;
-        var senha = listaEsperada[0].Senha;
-        var nick = listaEsperada[0].NickName;
-
-        var lista = usuarioRepositorio.ObterTodos();
-
-        Assert.NotEmpty(lista);
-        Assert.Equal(nome, lista[0].Nome);
-        Assert.Equal(id, lista[0].IdUsuario);
-        Assert.Equal(senha, lista[0].Senha);
-        Assert.Equal(nick, lista[0].NickName);
+        Assert.Equal(listaEsperada.Count(), lista.Count());
     }
 }
---
+++ Cod3rsGrowth.Teste/TestesUnitarios/TesteUsuarioRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Cod3rsGrowth.Dominio.Modelos;
+using Cod3rsGrowth.Teste.ClassesSingleton;
+using Cod3rsGrowth.Infra.Interfaces;
+
+namespace Cod3rsGrowth.Teste.TestesUnitarios;
+
+public class TesteUsuarioRepositorio : TesteBase
+{
+    private readonly IUsuarioRepositorio usuarioRepositorio;
+    public TesteUsuarioRepositorio()
+    {
+        usuarioRepositorio = serviceProvider.GetService<IUsuarioRepositorio>() ?? throw new Exception("Repositorio nao encontrado");
+    }
+
+    [Fact]
+    public void ao_ObterTodos_retorna_lista_com_quatro_usuarios()
+    {
+        usuarioRepositorio.Inserir(new() {Nome = "Gabriel Detofol", IdUsuario = 1, Plano = PlanoEnum.Premium, Senha = "123", NickName = "Detofol29"});
+        usuarioRepositorio.Inserir(new() {Nome = "Marcos Paulo", IdUsuario = 2, Plano = PlanoEnum.Nerd, Senha = "123", NickName = "SilvaMarcosPaulo"});
+        usuarioRepositorio.Inserir(new() {Nome = "Rubens", IdUsuario = 3, Plano = PlanoEnum.Kids, Senha = "123", NickName = "DarthRubens"});
+        usuarioRepositorio.Inserir(new() {Nome = "André", IdUsuario = 4, Plano = PlanoEnum.Free, Senha = "123", NickName = "AndrezinDoGrau"});
+        var listaEsperada = TabelasSingleton.ObterInstanciaUsuarios;
+
+        var lista = usuarioRepositorio.ObterTodos();
+
+        Assert.NotEmpty(lista);
+        Assert.Equal(listaEsperada.Count(), lista.Count());
+    }
+}
