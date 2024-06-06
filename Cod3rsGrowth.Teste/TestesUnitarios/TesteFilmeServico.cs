4d171f4dea7e26fdf986977ade84bf9cb561135b
Gabriel Detofol <detofol29@gmail.com>
2024-06-06 11:16:41 -0300

    (criar-metodo-obter-por-id-com-tdd) Criar teste para obter por id #time 0h

--- Cod3rsGrowth.Teste/TestesUnitarios/TesteFilmeServico.cs
+++ Cod3rsGrowth.Teste/TestesUnitarios/TesteFilmeServico.cs
@@ -14,9 +14,16 @@ public class TesteFilmeServico : TesteBase
         _servicos = serviceProvider.GetService<FilmeServicos>() ?? throw new Exception("servico nao encontrado");
     }
 
+    private Filme ObterFilmeEsperado()
+    {
+        return new Filme() { Id = 3, Titulo = "Star Wars", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre };
+    }
+
     [Fact]
     public void ao_ObterTodos_retorna_lista_com_doze_filmes()
     {
+        const int valorEsperado = 12;
+
         _servicos.Inserir(new Filme { Id = 1, Titulo = "De Volta Para o Futuro", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre});
         _servicos.Inserir(new Filme { Id = 2, Titulo = "Titanic", Genero = GeneroEnum.Romance, Classificacao = ClassificacaoIndicativa.doze});
         _servicos.Inserir(new Filme { Id = 3, Titulo = "Star Wars", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre});
@@ -31,9 +38,52 @@ public class TesteFilmeServico : TesteBase
         _servicos.Inserir(new Filme { Id = 12, Titulo = "O Cavaleiro das Trevas", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.quatorze});
 
         var lista = _servicos.ObterTodos();
-        var valorEsperado = 12;
 
         Assert.NotEmpty(lista);
         Assert.Equal(valorEsperado, lista.Count());
+    }
+
+    [Fact]
+    public void ao_ObterPorId_retorna_filme_com_id_igual_a_3()
+    {
+        _servicos.Inserir(new Filme { Id = 1, Titulo = "De Volta Para o Futuro", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre });
+        _servicos.Inserir(new Filme { Id = 2, Titulo = "Titanic", Genero = GeneroEnum.Romance, Classificacao = ClassificacaoIndicativa.doze });
+        _servicos.Inserir(new Filme { Id = 3, Titulo = "Star Wars", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre });
+        _servicos.Inserir(new Filme { Id = 4, Titulo = "O Senhor dos Anéis: A Sociedade do Anel", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 5, Titulo = "O Senhor dos Anéis: As Duas Torres", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 6, Titulo = "O Senhor dos Anéis: O Retorno do Rei", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 7, Titulo = "Matrix", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.dezesseis });
+        _servicos.Inserir(new Filme { Id = 8, Titulo = "Gladiador", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.dezesseis });
+        _servicos.Inserir(new Filme { Id = 9, Titulo = "O Poderoso Chefão", Genero = GeneroEnum.Drama, Classificacao = ClassificacaoIndicativa.dezoito });
+        _servicos.Inserir(new Filme { Id = 10, Titulo = "Forrest Gump", Genero = GeneroEnum.Drama, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 11, Titulo = "Pulp Fiction", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.dezoito });
+        _servicos.Inserir(new Filme { Id = 12, Titulo = "O Cavaleiro das Trevas", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.quatorze });
+        var filmeEsperado = ObterFilmeEsperado();
+
+        var filmeEncontrado = _servicos.ObterPorId(3);
+
+        Assert.NotNull(filmeEncontrado);
+        Assert.Equivalent(filmeEsperado, filmeEncontrado);
+    }
+
+    public void ao_ObterPorId_retorna_mensagem_de_erro_quando_id_nao_encontrado()
+    {
+        _servicos.Inserir(new Filme { Id = 1, Titulo = "De Volta Para o Futuro", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre });
+        _servicos.Inserir(new Filme { Id = 2, Titulo = "Titanic", Genero = GeneroEnum.Romance, Classificacao = ClassificacaoIndicativa.doze });
+        _servicos.Inserir(new Filme { Id = 3, Titulo = "Star Wars", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.livre });
+        _servicos.Inserir(new Filme { Id = 4, Titulo = "O Senhor dos Anéis: A Sociedade do Anel", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 5, Titulo = "O Senhor dos Anéis: As Duas Torres", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 6, Titulo = "O Senhor dos Anéis: O Retorno do Rei", Genero = GeneroEnum.Fantasia, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 7, Titulo = "Matrix", Genero = GeneroEnum.Ficcao, Classificacao = ClassificacaoIndicativa.dezesseis });
+        _servicos.Inserir(new Filme { Id = 8, Titulo = "Gladiador", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.dezesseis });
+        _servicos.Inserir(new Filme { Id = 9, Titulo = "O Poderoso Chefão", Genero = GeneroEnum.Drama, Classificacao = ClassificacaoIndicativa.dezoito });
+        _servicos.Inserir(new Filme { Id = 10, Titulo = "Forrest Gump", Genero = GeneroEnum.Drama, Classificacao = ClassificacaoIndicativa.quatorze });
+        _servicos.Inserir(new Filme { Id = 11, Titulo = "Pulp Fiction", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.dezoito });
+        _servicos.Inserir(new Filme { Id = 12, Titulo = "O Cavaleiro das Trevas", Genero = GeneroEnum.Acao, Classificacao = ClassificacaoIndicativa.quatorze });
+
+        var mensagemEsperada = "Filme nao encontrado";
+        var excecao = Assert.Throws<Exception>(() => _servicos.ObterPorId(13));
+
+        Assert.Equal(mensagemEsperada, excecao.Message);
     }
 }
--- Cod3rsGrowth.Teste/TestesUnitarios/TesteUsuarioServico.cs
+++ Cod3rsGrowth.Teste/TestesUnitarios/TesteUsuarioServico.cs
@@ -14,6 +14,11 @@ public class TesteUsuarioServico : TesteBase
         _servicos = serviceProvider.GetService<UsuarioServicos>() ?? throw new Exception("Servico nao encontrado");
     }
 
+    private Usuario ObterUsuarioEsperado()
+    {
+        return new Usuario() { Nome = "Rubens", IdUsuario = 3, Plano = PlanoEnum.Kids, Senha = "123", NickName = "DarthRubens" };
+    }
+
     [Fact]
     public void ao_ObterTodos_retorna_lista_com_quatro_usuarios()
     {
@@ -27,5 +32,33 @@ public class TesteUsuarioServico : TesteBase
 
         Assert.NotEmpty(lista);
         Assert.Equal(listaEsperada.Count(), lista.Count());
+    }
+
+    [Fact]
+    public void ao_ObterPorId_retorna_usuario_com_id_igual_a_3()
+    {
+        _servicos.Inserir(new() { Nome = "Gabriel Detofol", IdUsuario = 1, Plano = PlanoEnum.Premium, Senha = "123", NickName = "Detofol29" });
+        _servicos.Inserir(new() { Nome = "Marcos Paulo", IdUsuario = 2, Plano = PlanoEnum.Nerd, Senha = "123", NickName = "SilvaMarcosPaulo" });
+        _servicos.Inserir(new() { Nome = "Rubens", IdUsuario = 3, Plano = PlanoEnum.Kids, Senha = "123", NickName = "DarthRubens" });
+        _servicos.Inserir(new() { Nome = "André", IdUsuario = 4, Plano = PlanoEnum.Free, Senha = "123", NickName = "AndrezinDoGrau" });
+
+        var usuarioEncontrado = _servicos.ObterPorId(3);
+        var usuarioEsperado = ObterUsuarioEsperado();
+
+        Assert.Equivalent(usuarioEsperado, usuarioEncontrado);
+    }
+
+    [Fact]
+    public void ao_ObterPorId_retorna_mensagem_de_erro_quando_id_nao_encontrado()
+    {
+        _servicos.Inserir(new() { Nome = "Gabriel Detofol", IdUsuario = 1, Plano = PlanoEnum.Premium, Senha = "123", NickName = "Detofol29" });
+        _servicos.Inserir(new() { Nome = "Marcos Paulo", IdUsuario = 2, Plano = PlanoEnum.Nerd, Senha = "123", NickName = "SilvaMarcosPaulo" });
+        _servicos.Inserir(new() { Nome = "Rubens", IdUsuario = 3, Plano = PlanoEnum.Kids, Senha = "123", NickName = "DarthRubens" });
+        _servicos.Inserir(new() { Nome = "André", IdUsuario = 4, Plano = PlanoEnum.Free, Senha = "123", NickName = "AndrezinDoGrau" });
+
+        var mensagemEsperada = "Usuario nao encontrado";
+        var excecao = Assert.Throws<Exception>(() => _servicos.ObterPorId(5));
+
+        Assert.Equal(mensagemEsperada, excecao.Message);
     }
 }
