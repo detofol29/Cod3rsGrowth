6a69124b200c43d5fbe45e971739e8f9dffe4d5e
Gabriel Detofol <detofol29@gmail.com>
2024-07-18 13:51:01 -0300

    (criar-tela-de-lista) Refatorando código #time 30min

--- Cod3rsGrowth.Forms/Properties/Resources.Designer.cs
+++ Cod3rsGrowth.Forms/Properties/Resources.Designer.cs
@@ -99,15 +99,5 @@ namespace Cod3rsGrowth.Forms.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap perfilPenguin {
-            get {
-                object obj = ResourceManager.GetObject("perfilPenguin", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
