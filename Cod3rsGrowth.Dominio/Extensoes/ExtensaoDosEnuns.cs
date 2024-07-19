9ea07e7b224cb6c30e42c8cab146f6961dbf37a5
Gabriel Detofol <detofol29@gmail.com>
2024-07-19 10:57:14 -0300

    (criar-tela-de-lista) Alterando método de retorno de string #time 20h

--- Cod3rsGrowth.Dominio/Extensoes/ExtensaoDosEnuns.cs
+++ Cod3rsGrowth.Dominio/Extensoes/ExtensaoDosEnuns.cs
@@ -1,10 +1,5 @@
 ﻿using Cod3rsGrowth.Dominio.Modelos;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cod3rsGrowth_Domínio.Extensoes
 {
