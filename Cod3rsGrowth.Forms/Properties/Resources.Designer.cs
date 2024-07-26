73ab975d69e8314d08009e047dfb74c5f47a8edc
Gabriel Detofol <detofol29@gmail.com>
2024-07-26 09:18:51 -0300

    (criar-tela-de-criacao-com-tratativa-de-excecoes) Realizando mudancas solicitadas #time 1h

--- Cod3rsGrowth.Forms/Properties/Resources.Designer.cs
+++ Cod3rsGrowth.Forms/Properties/Resources.Designer.cs
@@ -99,15 +99,5 @@ namespace Cod3rsGrowth.Forms.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap olhopng {
-            get {
-                object obj = ResourceManager.GetObject("olhopng", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
